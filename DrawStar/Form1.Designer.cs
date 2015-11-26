4dcd480cae9432ce17ede10cb6e7f8e13d8f0d26
anjaliukrani <anjaukra@ed.amdsb.ca>
2015-11-26 13:53:17 -0500

    Finished Draw Star Program - Anjali Ukrani

---
+++ DrawStar/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DrawStar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawButton = new System.Windows.Forms.Button();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.xInput = new System.Windows.Forms.TextBox();
+            this.sizeInput = new System.Windows.Forms.TextBox();
+            this.yInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(590, 83);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(82, 23);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.xLabel.Location = new System.Drawing.Point(556, 5);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(14, 13);
+            this.xLabel.TabIndex = 1;
+            this.xLabel.Text = "X";
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.yLabel.Location = new System.Drawing.Point(556, 34);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(14, 13);
+            this.yLabel.TabIndex = 2;
+            this.yLabel.Text = "Y";
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.sizeLabel.Location = new System.Drawing.Point(522, 60);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(62, 13);
+            this.sizeLabel.TabIndex = 3;
+            this.sizeLabel.Text = "Size (pixels)";
+            // 
+            // xInput
+            // 
+            this.xInput.Location = new System.Drawing.Point(590, 2);
+            this.xInput.Name = "xInput";
+            this.xInput.Size = new System.Drawing.Size(82, 20);
+            this.xInput.TabIndex = 0;
+            // 
+            // sizeInput
+            // 
+            this.sizeInput.Location = new System.Drawing.Point(590, 57);
+            this.sizeInput.Name = "sizeInput";
+            this.sizeInput.Size = new System.Drawing.Size(82, 20);
+            this.sizeInput.TabIndex = 4;
+            // 
+            // yInput
+            // 
+            this.yInput.Location = new System.Drawing.Point(590, 27);
+            this.yInput.Name = "yInput";
+            this.yInput.Size = new System.Drawing.Size(82, 20);
+            this.yInput.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.yInput);
+            this.Controls.Add(this.sizeInput);
+            this.Controls.Add(this.xInput);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.drawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.TextBox xInput;
+        private System.Windows.Forms.TextBox sizeInput;
+        private System.Windows.Forms.TextBox yInput;
+    }
+}
+
---
+++ DrawStar/Form1.cs
@@ -0,0 +1,63 @@
+﻿///Made by Anjali Ukrani
+/// On November 26, 2015
+/// Draw Star Program
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawStar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Draw star in different size and place 
+        /// </summary>
+        /// <param name="x"> x-position of star</param>
+        /// <param name="y">y-position of star</param>
+        /// <param name="size">size of star</param>
+        private void DrawStar(float x, float y, float size)
+        {
+            Pen yellowPen = new Pen(Color.LightGoldenrodYellow); //tool to create lines
+            Graphics formGraphics = this.CreateGraphics();       //to make lines
+           
+         
+            formGraphics.Clear(Color.Black);
+            formGraphics.DrawLine(yellowPen, (1 + x) * (size/208), (84 + y) * (size / 195), (84 + x) * (size/208), (84 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (84 + x) * (size / 208), (84 + y) * (size/195), (104 + x) * (size / 208), (9 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (104 + x) * (size / 208), (9 + y) * (size / 195), (129 + x) * (size / 208), (84 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (129 + x) * (size / 208), (84 + y) * (size / 195), (209 + x) * (size / 208), (83 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (209 + x) * (size / 208), (83 + y) * (size / 195), (143 + x) * (size / 208), (130 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (143 + x) * (size / 208), (130 + y) * (size / 195), (168 + x) * (size / 208), (204 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (168 + x) * (size / 208), (204 + y) * (size / 195), (104 + x) * (size / 208), (159 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (104 + x) * (size / 208), (159 + y) * (size / 195), (42 + x) * (size / 208), (204 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (42 + x) * (size / 208), (204 + y) * (size / 195), (66 + x) * (size / 208), (130 + y) * (size / 195));
+            formGraphics.DrawLine(yellowPen, (66 + x) * (size / 208), (130 + y) * (size / 195), (1 + x) * (size / 208), (84 + y) * (size / 195));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Graphics formGraphics = this.CreateGraphics();
+            float x = Convert.ToSingle(xInput.Text);              //holds x value inputed by user
+            float y = Convert.ToSingle(yInput.Text);              //holds y value inputed by user
+            float size = Convert.ToSingle(sizeInput.Text);        // holds inputed size value
+            Pen yellowPen = new Pen(Color.LightGoldenrodYellow);  //Tool to create star
+
+
+            //go to method: DrawStar
+            DrawStar(x, y, size);
+
+        }
+    }
+}
